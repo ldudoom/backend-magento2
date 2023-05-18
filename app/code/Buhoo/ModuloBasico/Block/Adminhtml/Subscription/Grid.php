391c656d9854f26e07566d7033737286308d7f52
Raul Chauvin <raul.chauvin.ojeda@gmail.com>
2023-05-18 17:38:04 -0500

    Finalizando configuracion del modelo

--- app/code/Buhoo/ModuloBasico/Block/Adminhtml/Subscription/Grid.php
+++ app/code/Buhoo/ModuloBasico/Block/Adminhtml/Subscription/Grid.php
@@ -5,28 +5,26 @@ namespace Buhoo\ModuloBasico\Block\Adminhtml\Subscription;
 use Magento\Backend\Block\Widget\Grid as WidgetGrid;
 use Magento\Backend\Block\Widget\Grid\Extended;
 
-//use Buhoo\ModuloBasico\Model\Resource\Subscription\Collection;
+use Buhoo\ModuloBasico\Model\ResourceModel\Subscription\Collection;
 
 
 class Grid extends Extended
 {
-    //protected Collection $_subscriptionCollection;
+    protected Collection $_subscriptionCollection;
 
     /**
      * @param \Magento\Backend\Block\Template\Context $context
      * @param \Magento\Backend\Helper\Data $backendHelper
-    // * @param \Buhoo\ModuloBasico\Model\ResourceModel\Subscription\Collection $subscriptionCollection
+     * @param \Buhoo\ModuloBasico\Model\ResourceModel\Subscription\Collection $subscriptionCollection
      * @param array $data
      */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
         \Magento\Backend\Helper\Data $backendHelper,
-      //  \Buhoo\ModuloBasico\Model\ResourceModel\Subscription\Collection $subscriptionCollection,
+        \Buhoo\ModuloBasico\Model\ResourceModel\Subscription\Collection $subscriptionCollection,
         array $data = []
     ){
-
-        //echo "oso";exit;
-       // $this->_subscriptionCollection = $subscriptionCollection;
+        $this->_subscriptionCollection = $subscriptionCollection;
         parent::__construct($context, $backendHelper, $data);
         $this->setEmptyText(__('No Subscription Found'));
     }
@@ -38,7 +36,7 @@ class Grid extends Extended
      */
     protected function _prepareCollection()
     {
-        //$this->setCollection($this->_subscriptionCollection);
+        $this->setCollection($this->_subscriptionCollection);
         return parent::_prepareCollection();
     }
 }
